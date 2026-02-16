2c6dd082edbd6193ced7711b55a4fd294c2e5216
abubekernuru <abubekernuru27@gmail.com>
2026-02-16 22:58:53 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="text-center text-2xl font-bold text-blue-600">
-      This is my 3rd mern full stach app built with react, node, express and mongoDB. I am learning how to build a full stack app with the mern stack and this is my first attempt at it. I am excited to learn more about the mern stack and how to build full stack apps with it.
+      mern blog web app
     </div>
   )
 }
