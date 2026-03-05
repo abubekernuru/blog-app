98743c95d6f6da5a996fd669cad0fbefe2bac1e8
abubekernuru <abubekernuru27@gmail.com>
2026-03-05 23:19:11 +0300

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 
 app.get('/', (req, res)=>{
     res.send("Hello Abuki check!")
 })
 
-app.listen(3002, ()=>{
-    console.log("app is listening on port 3002")
-})
+mongoose.connect('mongodb://localhost:27017/blog')
+    .then(()=>{
+        console.log("Succefully conected to db")
+        app.listen(3002, ()=>{
+            console.log("app is listening on port 3002")
+        })
+    })
