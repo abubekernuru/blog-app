29045923c810fe0b726aa38524846edaf190b2bd
abubekernuru <abubekernuru27@gmail.com>
2026-03-30 11:59:14 +0300

    add redux persist to save user data in localstorage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,15 @@ import './index.css'
 import 'flowbite'
 import { store } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
+
 createRoot(document.getElementById('root')).render(
+  <PersistGate loading={null} persistor={persistor}>
   <Provider store={store}>
     <App />
   </Provider>
+  </PersistGate>
 )
