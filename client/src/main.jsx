d6bed081248d0c0095f7649fe76c027706e62aaf
abubekernuru <abubekernuru27@gmail.com>
2026-05-02 23:34:47 +0300

    theme toggle fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,16 @@ import { store } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import { ThemeProvider } from './components/ThemeProvider.jsx';
 
 let persistor = persistStore(store);
 
 createRoot(document.getElementById('root')).render(
   <PersistGate loading={null} persistor={persistor}>
   <Provider store={store}>
+    <ThemeProvider
     <App />
+    <ThemeProvider
   </Provider>
   </PersistGate>
 )
